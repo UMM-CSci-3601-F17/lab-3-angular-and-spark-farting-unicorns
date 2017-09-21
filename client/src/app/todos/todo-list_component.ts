52478273daa2f91800198b5a7a5955ef444ff82a
M1NN <score@morris.umn.edu>
2017-09-21 14:26:22 -0500

    We added a todo-list.component.ts and we added 4 filter methods to it. Filter by owner, status, category, and body. This is for issue #2

---
+++ client/src/app/todos/todo-list_component.ts
@@ -0,0 +1,83 @@
+import {Component, OnInit} from '@angular/core';
+import {TodoListService} from "./todo-list.service";
+import {todo} from "./todo";
+
+@Component({
+    selector: 'todo-list-component',
+    templateUrl: 'todo-list.component.html',
+    providers: []
+})
+
+export class UserListComponent implements OnInit {
+    //These are public so that tests can reference them (.spec.ts)
+    public todos: todo[];
+    public filteredTodos: todo[];
+
+    //Inject the UserListService into this component.
+    //That's what happens in the following constructor.
+    //
+    //We can call upon the service for interacting
+    //with the server.
+    constructor(private todoListService: TodoListService) {
+
+    }
+
+    public filterTodos(searchOwner: string, searchStatus: boolean, searchCategory: string, searchBody: string): Todo[] {
+
+        this.filteredTodos = this.todos;
+
+        //Filter by owner
+        if (searchOwner != null) {
+            searchOwner = searchOwner.toLocaleLowerCase();
+
+            this.filteredTodos = this.filteredTodos.filter(todo => {
+                return !searchOwner || todo.owner.toLowerCase().indexOf(searchOwner) !== -1;
+            });
+        }
+
+        //Filter by status
+        if (searchStatus != null) {
+            this.filteredTodos = this.filteredTodos.filter(todo => {
+                return !searchStatus || todo.status == searchStatus;
+            });
+        }
+
+        //Filter by category
+        if (searchCategory != null) {
+            searchCategory = searchCategory.toLocaleLowerCase();
+
+            this.filteredTodos = this.filteredTodos.filter(todo => {
+                return !searchOwner || todo.category.toLowerCase().indexOf(searchCategory) !== -1;
+            })
+        }
+
+        //Filter by body
+        if(searchBody != null) {
+            searchBody = searchBody.toLocaleLowerCase();
+
+            this.filteredTodos = this.filteredTodos.filter(todo => {
+                return !searchBody || todo.body.toLowerCase().indexOf(searchBody) !== -1;
+            })
+        }
+
+
+        return this.filteredTodos;
+    }
+
+    ngOnInit(): void {
+        //Get Users returns an Observable, basically a "promise" that
+        //we will get the data from the server.
+        //
+        //Subscribe waits until the data is fully downloaded, then
+        //performs an action on it (the first lambda)
+        this.todoListService.getTodos().subscribe(
+            todos => {
+                this.todos = todos;
+                this.filteredTodos = this.todos;
+            },
+            err => {
+                console.log(err);
+            }
+        );
+    }
+}
